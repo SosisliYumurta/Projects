215982a5f61ae701bfa1f4cd23ceb08b7dec8445
Oguz Berk <ouzberkaydin@gmail.com>
2023-01-20 16:24:08 +0100

    abstract method add

---
+++ C#/App_Netwrok_Abstract/App_Netwrok_Abstract/Device.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Netwrok_Abstract
+{
+	public abstract class Device
+	{
+		public Device(string name, string make)
+		{
+			_name = name;
+			_make = make;
+		}
+
+		protected string _name;
+		protected string _make;
+		public abstract void StartDevice();
+		public abstract void ShutDown();
+
+	}
+}
